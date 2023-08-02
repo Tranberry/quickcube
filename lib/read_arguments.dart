a4c3f38592c4e6868042ee0632054d71db7bfb4f
TRBRY <oskar@tranberry.se>
2023-08-02 23:16:06 +0200

    sanitized user input so they don't do anything they do not want too

--- lib/read_arguments.dart
+++ lib/read_arguments.dart
@@ -1,28 +1,40 @@
 import 'dart:io';
-
 import 'tool_name.dart';
 
 Map<String, String> readArguments(List<String> arguments) {
-  // current directory
+  // Sanitize the arguments so we do not try to make invalid directories or include odd symbols
+
+  // Current directory
   final currentDirectory = Directory.current;
-  // make sure the are not empty
+
   if (arguments.isEmpty) {
-    // exit without error
+    // Exit without error
     stdout.writeln('$toolName, a simple project creator for SugarCube.');
     stdout.writeln('');
     stdout.writeln('You need to name the project like this example:');
     stdout.writeln('  > ${toolName.toLowerCase()} my sugarcube project');
     stdout.writeln('');
-    // explain that this would create an empty project directory
+    // Explain that this would create an empty project directory
     stdout.writeln(
         'This would create a "my_sugarcube_project" directory in: \n"${currentDirectory.path}".');
-    stdout.writeln('With directories, files and some quality of life things');
+    stdout.writeln('With directories, files, and some quality of life things');
     exit(0);
   } else {
-    // combine the arguments into snakecase
+    // Combine the arguments into snake case and sanitize
+    final projectName =
+        arguments.join(' ').trim().replaceAll(RegExp(r'[^\w\s]'), '');
+    final projectDirectory =
+        projectName.replaceAll(RegExp(r'\s+'), '_').toLowerCase();
+
+    if (projectName.isEmpty) {
+      // Handle empty project name after sanitization
+      stderr.writeln('Error: Project name cannot be empty.');
+      exit(1);
+    }
+
     return {
-      'ProjectName': arguments.join(' '),
-      'ProjectDirectory': arguments.join('_').toLowerCase()
+      'ProjectName': projectName,
+      'ProjectDirectory': projectDirectory,
     };
   }
 }
