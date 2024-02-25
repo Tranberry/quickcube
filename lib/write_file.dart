956e7932f5c638cda1a8be94e7610de7f5354813
TRBRY <thissoda@gmail.com>
2024-02-25 18:37:06 +0100

    added docs

--- lib/write_file.dart
+++ lib/write_file.dart
@@ -1,5 +1,9 @@
 import 'dart:io';
 
+/// Writes the given payload to the file at the specified path.
+///
+/// * [path]: The path to the file.
+/// * [payload]: The content to be written to the file.
 void writeFile(String path, String payload) {
   File file = File(path);
   file.writeAsStringSync(payload);
