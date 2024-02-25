81d92f0c850106947486e99f7e454daab7ea726f
TRBRY <thissoda@gmail.com>
2024-02-25 18:35:42 +0100

    add typeDef to package.json

--- bin/quickcube.dart
+++ bin/quickcube.dart
@@ -4,14 +4,12 @@ import 'package:path/path.dart' as path;
 import 'package:quickcube/read_arguments.dart';
 import 'package:quickcube/simple_file_content.dart';
 import 'package:quickcube/twee_start.dart';
-
 import 'package:quickcube/print_helpful_commands.dart';
 import 'package:quickcube/write_file.dart';
 
-void main(List<String> arguments) {
+void main(List<String> arguments) async {
   final projectName = readArguments(arguments);
   // read arguments and make it into a system valid director name (project name).
-
   Directory projectDirectory = Directory(projectName['ProjectDirectory']!);
   Directory srcDirectory = Directory(path.join(projectDirectory.path, 'src'));
   Directory distDirectory = Directory(path.join(projectDirectory.path, 'dist'));
@@ -22,10 +20,12 @@ void main(List<String> arguments) {
 
   // create directories if they do not exist
   if (!projectDirectory.existsSync()) {
+    stdout.writeln('');
     stdout.writeln(
         'Creating your new SugarCube project "${projectName['ProjectName']}"');
     stdout.writeln('');
     stdout.writeln('Creating DIRECTORIES');
+
     // create directories
     // create projectDirectory
     projectDirectory.createSync(recursive: true);
@@ -42,9 +42,9 @@ void main(List<String> arguments) {
     // create assetsDirectory
     assetsDirectory.createSync(recursive: true);
     stdout.writeln('"${assetsDirectory.absolute.path}"');
-
     stdout.writeln('');
     stdout.writeln('Creating FILES');
+
     // create files
     // create js config in src directory with content of `jsConfig`
     writeFile(path.join(srcDirectory.path, 'config.js'), jsConfig);
@@ -72,7 +72,14 @@ void main(List<String> arguments) {
     stdout.writeln(
         '"${assetsDirectory.absolute.path}${Platform.pathSeparator}assets.txt"');
 
+    // Write a `package.json` file in the root of the project
+    writeFile(
+        path.join(projectDirectory.path, 'package.json'), nodePackageContent);
+    stdout.writeln(
+      '"${projectDirectory.absolute.path}${Platform.pathSeparator}package.json"',
+    );
     // end of generation message
+
     printHelpfulCommands(projectDirectory);
   } else {
     stdout.writeln('Directory already exists');
