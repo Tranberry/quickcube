d09d3a879048d94d24af3907151b69d5c9b43fd1
TRBRY <thissoda@gmail.com>
2024-02-25 16:53:46 +0100

    added doc

--- lib/ifid.dart
+++ lib/ifid.dart
@@ -1,6 +1,8 @@
 import 'package:uuid/uuid.dart';
 
-/// Generates a random IFID string
+/// Generates and returns a unique identifier in uppercase.
+///
+/// `IFID` is a interactive fiction identifier.
 String ifid() {
   var uuid = Uuid();
   return uuid.v4().toUpperCase();
