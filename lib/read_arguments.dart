3fa067f77f332dbdf6d13db96e6f3641b00c04bc
TRBRY <thissoda@gmail.com>
2024-02-25 16:51:42 +0100

    refactor and docs added

--- lib/read_arguments.dart
+++ lib/read_arguments.dart
@@ -1,35 +1,28 @@
 import 'dart:io';
 import 'tool_name.dart';
 
+/// Reads the command line arguments and returns a map containing the project name and directory.
+///
+/// This function takes a list of string arguments and processes them to extract the project name and directory.
+/// If the arguments list is empty, it prints project creation instructions and exits the program.
+///
+/// Parameters:
+///   arguments - List of string arguments from the command line
+///
+/// Returns:
+///   A map containing the 'ProjectName' and 'ProjectDirectory'
 Map<String, String> readArguments(List<String> arguments) {
-  // Sanitize the arguments so we do not try to make invalid directories or include odd symbols
-
-  // Current directory
   final currentDirectory = Directory.current;
 
   if (arguments.isEmpty) {
-    // Exit without error
-    stdout.writeln('$toolName, a simple project creator for SugarCube.');
-    stdout.writeln('');
-    stdout.writeln('You need to name the project like this example:');
-    stdout.writeln('  > ${toolName.toLowerCase()} my sugarcube project');
-    stdout.writeln('');
-    // Explain that this would create an empty project directory
-    stdout.writeln(
-        'This would create a "my_sugarcube_project" directory in: \n"${currentDirectory.path}".');
-    stdout.writeln('With directories, files, and some quality of life things');
+    printProjectCreationInstructions(toolName, currentDirectory);
     exit(0);
   } else {
-    // Combine the arguments into snake case and sanitize
-    final projectName =
-        arguments.join(' ').trim().replaceAll(RegExp(r'[^\w\s]'), '');
-    final projectDirectory =
-        projectName.replaceAll(RegExp(r'\s+'), '_').toLowerCase();
+    final projectName = sanitizeProjectName(arguments);
+    final projectDirectory = formatProjectDirectory(projectName);
 
     if (projectName.isEmpty) {
-      // Handle empty project name after sanitization
-      stderr.writeln('Error: Project name cannot be empty.');
-      exit(1);
+      printErrorAndExit('Error: Project name cannot be empty.');
     }
 
     return {
@@ -37,4 +30,51 @@ Map<String, String> readArguments(List<String> arguments) {
       'ProjectDirectory': projectDirectory,
     };
   }
+}
+
+/// Print project creation instructions.
+///
+/// This function prints instructions for creating a project with the specified tool name and current directory.
+///
+/// Parameters:
+///   toolName - The name of the tool
+///   currentDirectory - The current directory
+void printProjectCreationInstructions(
+    String toolName, Directory currentDirectory) {
+  stdout.writeln('');
+  stdout.writeln('$toolName, a simple project creator for SugarCube.');
+  stdout.writeln('');
+
+  // Print project naming example
+  stdout.writeln('You need to name the project like this example:');
+  stdout.writeln('  > ${toolName.toLowerCase()} my sugarcube project');
+  stdout.writeln('');
+
+  // Print project creation directory
+  stdout.writeln(
+      'This would create a "my_sugarcube_project" directory in: \n"${currentDirectory.path}".');
+  stdout.writeln('With directories, files, and some quality of life things');
+  stdout.writeln('');
+}
+
+/// Sanitize the project name by joining the arguments, trimming any leading or trailing whitespace, and removing any non-alphanumeric characters.
+///
+/// arguments: list of strings to sanitize
+String sanitizeProjectName(List<String> arguments) {
+  return arguments.join(' ').trim().replaceAll(RegExp(r'[^\w\s]'), '');
+}
+
+/// Formats the project directory by replacing spaces with underscores and converting to lowercase.
+///
+/// [projectName]: The name of the project to format.
+String formatProjectDirectory(String projectName) {
+  return projectName.replaceAll(RegExp(r'\s+'), '_').toLowerCase();
+}
+
+/// Prints the given error message to the standard error stream and exits the program with a status code of 1.
+///
+/// [errorMessage] - The error message to be printed.
+void printErrorAndExit(String errorMessage) {
+  stderr.writeln(errorMessage);
+  exit(1);
 }
