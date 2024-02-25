d54f74e713879c94cd9944b90fbcee5686c356f7
TRBRY <thissoda@gmail.com>
2024-02-25 18:36:41 +0100

    small QoL update

--- lib/print_helpful_commands.dart
+++ lib/print_helpful_commands.dart
@@ -1,7 +1,20 @@
 import 'dart:io';
+import 'check_path_for_app.dart';
 
+// a list of names for different VS Code editions
+const List<String> codeEditors = ['code', 'code-insiders'];
+
+var gotCode = checkPathForApps(codeEditors);
+
+/// Prints helpful commands for the given project directory.
+///
+/// If [gotCode] is true, it prints the command to change to the project directory and execute the specified command.
+/// Otherwise, it just prints the command to change to the project directory.
 void printHelpfulCommands(Directory projectDirectory) {
-  // print something like 'cd your_project_directory' and 'code .' where your_project_directory is the project directory
-  stdout.writeln('');
-  stdout.writeln('    cd ${projectDirectory.path} && code .');
+  if (gotCode[0]) {
+    stdout.writeln('');
+    stdout.writeln('    cd ${projectDirectory.path} && ${gotCode[1]} .');
+  } else {
+    stdout.writeln('    cd ${projectDirectory.path}');
+  }
 }
